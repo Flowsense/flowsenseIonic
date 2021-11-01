11cc5974bdf1d954b3853cab9a40689fa684b451
Feller <feller.ailton@edu.univali.br>
2021-11-01 09:22:36 -0300

    from mac mini to macbook

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 
+declare var Flowsense;
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -22,6 +24,43 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      let flowsense = new Flowsense("b9f3bd67e0694426a441e6528c2c8c91");
+
+      flowsense.startSDK();
+      flowsense.requestAlwaysAuthorization();
+      flowsense.startMonitoringLocation();
+      flowsense.updatePartnerUserId("testeionic0803");
+      flowsense.pushNotificationsEnabled(true);
+      flowsense.smsEnabled(false);
+      flowsense.emailEnabled(true);
+      flowsense.createNotificationChannel("fs_ionic_channel");
+      if (this.platform.is('ios')) {
+        flowsense.requestPushToken();
+      }
+
+      const map = {
+        "nomeBotaoClicado": "Botao Enviar",
+        "dataClique": new Date()
+      };
+
+      flowsense.inAppEvent("clique_de_botao", map);
+      flowsense.inAppEventLocalized("clique_de_botao_ios", map);
+
+
+      flowsense.onNotification((jsonData) => {
+        console.log(jsonData);
+      });
+
+      const notificationOpenedCallback = (jsonData) => {
+        console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+      };
+
+      // window["plugins"].OneSignal
+      //   .startInit("4f07869f-ea3c-4c60-b42e-0707d4e9e9dd", "895702690058")
+      //   .handleNotificationOpened(notificationOpenedCallback)
+      //   .endInit();
+
     });
   }
 }
